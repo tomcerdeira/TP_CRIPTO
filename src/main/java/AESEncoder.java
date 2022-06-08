39831ba5de0098949d1fe9d0080a392d1de16dbe
santigarciam <sangarcia@itba.edu.ar>
2022-06-08 16:23:28 -0300

    Start impl of AES 128

---
+++ src/main/java/AESEncoder.java
@@ -0,0 +1,86 @@
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+
+public class AESEncoder {    public void LSBImproved(){
+        //TODO
+    }
+
+
+    public static SecretKey getKeyFromPassword(String password, String salt)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
+
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), 65536, 256);
+        return new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "AES");
+    }
+
+
+    public static IvParameterSpec generateIv() {
+        byte[] iv = new byte[16];
+        new SecureRandom().nextBytes(iv);
+        return new IvParameterSpec(iv);
+    }
+
+    public static void encryptFile(String algorithm, SecretKey key, IvParameterSpec iv,
+                                   File inputFile, File outputFile) throws IOException, NoSuchPaddingException,
+            NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException,
+            BadPaddingException, IllegalBlockSizeException {
+
+        Cipher cipher = Cipher.getInstance(algorithm);
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+        FileInputStream inputStream = new FileInputStream(inputFile);
+        FileOutputStream outputStream = new FileOutputStream(outputFile);
+        byte[] buffer = new byte[64];
+        int bytesRead;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            byte[] output = cipher.update(buffer, 0, bytesRead);
+            if (output != null) {
+                outputStream.write(output);
+            }
+        }
+        byte[] outputBytes = cipher.doFinal();
+        if (outputBytes != null) {
+            outputStream.write(outputBytes);
+        }
+        inputStream.close();
+        outputStream.close();
+    }
+
+
+    public static void decryptFile(String algorithm, SecretKey key, IvParameterSpec iv,
+                                   File inputFile, File outputFile) throws IOException, NoSuchPaddingException,
+            NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException,
+            BadPaddingException, IllegalBlockSizeException {
+
+        Cipher cipher = Cipher.getInstance(algorithm);
+        cipher.init(Cipher.DECRYPT_MODE, key, iv);
+        FileInputStream inputStream = new FileInputStream(inputFile);
+        FileOutputStream outputStream = new FileOutputStream(outputFile);
+        byte[] buffer = new byte[64];
+        int bytesRead;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            byte[] output = cipher.update(buffer, 0, bytesRead);
+            if (output != null) {
+                outputStream.write(output);
+            }
+        }
+        byte[] outputBytes = cipher.doFinal();
+        if (outputBytes != null) {
+            outputStream.write(outputBytes);
+        }
+        inputStream.close();
+        outputStream.close();
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,44 @@
+import exceptions.FileTooLargetException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.Objects;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedImage bufferedImage = ImageIO.read(Objects.requireNonNull(Main.class.getResource("testImage.bmp")));
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
+//        BufferedImage bufferedImage = ImageIO.read(Objects.requireNonNull(Main.class.getResource("testImage.bmp")));
+//        byte[] arr = new String("HOla").getBytes(StandardCharsets.UTF_8);
+//        BMPSteganographEncoder steganograph = new BMPSteganographEncoder(bufferedImage, arr);
+//        try {
+//            steganograph.LSB1();
+//            steganograph.getEditor().outputToFile("escondeHola.bmp");
+//        } catch (FileTooLargetException e) {
+//            e.printStackTrace();
+//        }
 
-        BMPSteganographEncoder steganograph = new BMPSteganographEncoder(bufferedImage, null);
+
+        AESEncoder encoder = new AESEncoder();
+        SecretKey key = AESEncoder.getKeyFromPassword("password", "salt");
+        String algo = "AES/CBC/PKCS5Padding";
+        IvParameterSpec ivParameterSpec = AESEncoder.generateIv();
+        File file = new File("/home/santiago/Desktop/TP_CRIPTO/src/main/resources/testImage.bmp");
+        File outputFile = new File("/home/santiago/Desktop/TP_CRIPTO/src/main/resources/testImageEncry.bmp");
+        AESEncoder.encryptFile(algo,key,ivParameterSpec,file,outputFile);
+        AESEncoder.decryptFile(algo,key,ivParameterSpec,outputFile,file);
+
 
     }
 
